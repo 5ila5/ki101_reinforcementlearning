7af47a3a2c6e8ddb28cf5e788d95efe35dbfbb55
5ila5 <5ila5@users.noreply.github.com>
2024-01-24 17:56:57 +0100

    example scene

---
+++ ai101_video/scenes/example.py
@@ -0,0 +1,31 @@
+from manim import *
+
+from ai101_video.default_voice_scene import DefaultMainVoiceScene
+
+
+class Example (DefaultMainVoiceScene):
+    ORDER = 1
+
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.rotate(PI / 4)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
+
+class Example2 (DefaultMainVoiceScene):
+    ORDER = 0
+
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+
+        square = Square()  # create a square
+        square.set_fill(BLUE, opacity=0.5)  # set the color and transparency
+
+        square.next_to(circle, RIGHT, buff=0.5)  # set the position
+        self.play(Create(circle), Create(square))  # show the shapes on screen
