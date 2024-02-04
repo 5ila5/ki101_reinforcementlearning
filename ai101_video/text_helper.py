3075fd2cc51bef31f5daec0d8018fba884a20f0b
joshbert1107 <Joshua11071107@gmail.com>
2024-02-04 11:52:24 +0100

    updated text for environment, also added last part: transition function

--- ai101_video/text_helper.py
+++ ai101_video/text_helper.py
@@ -19,13 +19,14 @@ class Text_Helper:
             "end": 'Nun ist es die Aufgabe des "agents" nach diesen Gegebenheiten eine optimale "policy" zu entwerfen, das heißt zu erlernen welche "action" man je nach "state" am besten wählt. Die Besonderheit von "Reinforcement Learning" Problemen ist, dass dem "agent" weder die "Reward-Function", noch die "Transition-Function" bekannt ist. Also muss er durch die Interaktion mit seiner Umgebung lernen. Dabei befindet er sich jedem Schritt in einem neuen "state" und erhält dazu passend einen "reward". Um seinen "reward" zu maximieren, arbeitet der "agent" eine immer bessere "policy" aus.',
         },
         "Praxis-Environment": {
-            "agent": "DIESER TEXT IST NOCH NICHT FERTIG, ER WIRD NOCH BEARBEITET, BITTE NICHT VERWENDEN, DANKE! EIN BEISPIEL WIRD NOCH EINGEFÜGT!",
+            "agent": "Um euch Schritt für Schritt zu erklären wie alles funktioniert, werden wir uns an einem Beispiel orientieren. Hier unser Szenario:Alles dreht sich um den sogenannten agent. Stellt euch das so vor wie eine Spielfigur in einem Videospiel",
             "title": "Praxis: Environment",
-            "start": "Jetzt ist es erstmal genug mit der Theorie. Wir wollen uns das Ganze jetzt mal an einem Beispiel anschauen.",
-            "real_start": "In diesem Szenario ist ein Elf zu sehen, der versucht, das Geschenk zu erreichen. Der Elf erhält dann eine Belohnung, wenn er es geschafft hat.",
-            "show_lake": "Dafür muss er sich über einen zugefrorenen See bewegen, indem er sich wie auf einem Schachbrett von Feld zu Feld bewegt.",
-            "slippery1": "Dabei muss er nicht nur den richtigen Weg finden und die Löcher in der Eisdecke umgehen, sondern auch damit umgehen, dass die Eisdecke rutschig ist.",
+            "start": "In diesem Szenario folgen wir einem Elfen, der versucht, ein Geschenk zu erreichen.",
+            "real_start": "Dafür muss er sich über einen zugefrorenen See bewegen, indem er sich wie auf einem Schachbrett von Feld zu Feld bewegt.",
+            "show_lake": "Jeder Schritt ist eine sogenannte action. Bei jeder action verändert sich der Zustand des Szenarios. Den nennt man state. Wichtig ist, dass der agent typischerweise kein Vorwissen über die Umgebung hat.",
+            "slippery1": "Dabei muss er nicht nur den richtigen Weg finden und die Löcher in der Eisdecke umgehen in dem er die richtige Abfolge von Schritten macht, sondern auch damit umgehen, dass die Eisdecke rutschig ist.",
             "slippery2": "Es kann also passieren, dass er statt in die gewählte Richtung auch in eine andere Richtung rutscht.",
+            "transtiton_function": 'Diese Definition von Übergängen zwischen den States nennt man "transition function"',
         },
     }
 
